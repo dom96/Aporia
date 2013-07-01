283b34f87d411940d473c618a1c5a8a61e1d7954
Thomas Kinnen <thomas.kinnen@gmail.com>
2013-07-01 16:11:15 +0200

    Use case..of syntax instead of lengthy if else

--- suggest.nim
+++ suggest.nim
@@ -39,9 +39,10 @@ proc addSuggestItem*(win: var MainWin, name: string, markup: String,
 proc addSuggestItem(win: var MainWin, item: TSuggestItem) =
   # TODO: Escape tooltip text for pango markup.
   var markup = "<b>$1</b>" % [escapePango(item.nmName)]
-  if item.nodeType == "skProc" or item.nodeType == "skTemplate" or item.nodeType == "skIterator":
+  case item.nodeType
+  of "skProc", "skTemplate", "skIterator":
     markup = "$1$2" % [escapePango(item.nmName), item.nimType.replaceWord("proc ","")]
-  elif item.nodeType == "skField":
+  of "skField":
     markup = "<i>$1 - $2</i>" % [escapePango(item.nmName), item.nimType]
   win.addSuggestItem(item.nmName, markup, item.nimType)
 
